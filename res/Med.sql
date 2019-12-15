5d2aae109ea664b9978e5af12af9c137d402f5ec
zanderthuo <thuozander@gmail.com>
2019-12-15 10:04:07 +0300

    Update Med.sql

--- res/Med.sql
+++ res/Med.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2019 at 11:10 AM
+-- Generation Time: Dec 15, 2019 at 07:55 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.1.31
 
@@ -114,29 +114,71 @@ INSERT INTO `bill_medicine` (`id`, `bill_id`, `product_id`, `batch_no`, `qty`) V
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `inn`
+--
+
+CREATE TABLE `inn` (
+  `Id` int(11) NOT NULL,
+  `meds` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inn`
+--
+
+INSERT INTO `inn` (`Id`, `meds`, `quantity`) VALUES
+(1, 'Ketto', 30),
+(2, 'Ketto', 30),
+(3, 'Ketto', 30),
+(4, 'Ketto', 30),
+(5, 'Ketto', 78),
+(6, 'Paracetemol', 78),
+(7, 'Paracetemol', 78),
+(8, 'Paracetemol', 78),
+(9, 'Paracetemol', 78),
+(10, 'Paracetemol', 78);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `medicine`
 --
 
 CREATE TABLE `medicine` (
   `product_id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
+  `name` varchar(20) NOT NULL,
+  `prices` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `medicine`
 --
 
-INSERT INTO `medicine` (`product_id`, `name`) VALUES
-(1, 'Aceclofenac '),
-(2, 'BournVita'),
-(3, 'Digene'),
-(4, 'Flexon'),
-(5, 'Gelucil'),
-(6, 'Horlicks'),
-(7, 'Ketto'),
-(8, 'Lopex'),
-(9, 'Paracentamol'),
-(10, 'Aceclofenac 2');
+INSERT INTO `medicine` (`product_id`, `name`, `prices`) VALUES
+(1, 'Aceclofenac ', 0),
+(2, 'BournVita', 0),
+(3, 'Digene', 0),
+(4, 'Flexon', 0),
+(5, 'Gelucil', 0),
+(6, 'Horlicks', 0),
+(7, 'Ketto', 0),
+(8, 'Lopex', 0),
+(9, 'Paracentamol', 0),
+(10, 'Aceclofenac 2', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `NewPres`
+--
+
+CREATE TABLE `NewPres` (
+  `Id` int(11) NOT NULL,
+  `patientname` varchar(255) NOT NULL,
+  `medicinename` varchar(255) NOT NULL,
+  `dosage` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -146,7 +188,7 @@ INSERT INTO `medicine` (`product_id`, `name`) VALUES
 
 CREATE TABLE `ordered_medicine` (
   `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
+  `medicine_name` varchar(255) NOT NULL,
   `product_id` int(11) NOT NULL,
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -155,19 +197,27 @@ CREATE TABLE `ordered_medicine` (
 -- Dumping data for table `ordered_medicine`
 --
 
-INSERT INTO `ordered_medicine` (`id`, `order_id`, `product_id`, `qty`) VALUES
-(1, 1, 1, 100),
-(2, 1, 1, 30),
-(3, 2, 1, 100),
-(4, 2, 2, 10),
-(5, 3, 3, 20),
-(6, 4, 7, 2),
-(7, 5, 7, 2),
-(8, 6, 7, 2),
-(9, 7, 7, 1),
-(10, 8, 7, 2),
-(11, 9, 7, 2),
-(12, 10, 9, 10);
+INSERT INTO `ordered_medicine` (`id`, `medicine_name`, `product_id`, `qty`) VALUES
+(1, '1', 1, 100),
+(2, '1', 1, 30),
+(3, '2', 1, 100),
+(4, '2', 2, 10),
+(5, '3', 3, 20),
+(6, '4', 7, 2),
+(7, '5', 7, 2),
+(8, '6', 7, 2),
+(9, '7', 7, 1),
+(10, '8', 7, 2),
+(11, '9', 7, 2),
+(12, '10', 9, 10),
+(13, '11', 7, 5),
+(14, '12', 7, 5),
+(15, '13', 7, 6),
+(16, '14', 7, 5),
+(17, '14', 9, 89),
+(18, '15', 9, 40),
+(19, '16', 9, 40),
+(20, '17', 9, 45);
 
 -- --------------------------------------------------------
 
@@ -192,14 +242,24 @@ CREATE TABLE `orders` (
 INSERT INTO `orders` (`order_id`, `shop_id`, `supplier_id`, `seen`, `processed`, `confirmed`, `date`) VALUES
 (1, 'koshal_sk@gmail.com', 'koshal_s@gmail.com', 1, 1, 1, '2018-04-12'),
 (2, 'koshal_sk@gmail.com', 'koshal_s@gmail.com', 1, 1, 1, '2018-04-12'),
-(3, 'koshal_sk@gmail.com', '', 0, 0, 0, '2019-11-25'),
-(4, 'dibya_sk@gmail.com', '', 0, 0, 0, '2019-11-25'),
-(5, 'dibya_sk@gmail.com', '', 0, 0, 0, '2019-11-25'),
-(6, 'dibya_sk@gmail.com', '', 0, 0, 0, '2019-11-25'),
+(3, 'koshal_sk@gmail.com', '', 1, 0, 0, '2019-11-25'),
+(4, 'dibya_sk@gmail.com', '', 1, 0, 0, '2019-11-25'),
+(5, 'dibya_sk@gmail.com', '', 1, 0, 0, '2019-11-25'),
+(6, 'dibya_sk@gmail.com', '', 1, 0, 0, '2019-11-25'),
 (7, 'dibya_sk@gmail.com', 'test2@test.com', 1, 1, 1, '2019-11-25'),
 (8, 'test11@test.com', 'test2@test.com', 1, 1, 0, '2019-11-25'),
 (9, 'test11@test.com', 'test2@test.com', 1, 0, 0, '2019-11-25'),
-(10, 'test3@gmail.com', 'joy@gmail.com', 1, 1, 1, '2019-11-30');
+(10, 'test3@gmail.com', 'joy@gmail.com', 1, 1, 1, '2019-11-30'),
+(11, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-12'),
+(12, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-12'),
+(13, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-12'),
+(14, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-12'),
+(15, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-12'),
+(16, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-12'),
+(17, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-13'),
+(18, 'alex@gmail.com', '', 0, 0, 0, '2019-12-13'),
+(19, 'alex@gmail.com', '', 0, 0, 0, '2019-12-13'),
+(20, 'alex@gmail.com', 'test2@test.com', 1, 0, 0, '2019-12-13');
 
 -- --------------------------------------------------------
 
@@ -260,6 +320,7 @@ INSERT INTO `pres_meds` (`id`, `pres_id`, `product_id`, `quantity`) VALUES
 CREATE TABLE `stock` (
   `shop_id` varchar(100) NOT NULL,
   `product_id` int(11) NOT NULL,
+  `medicine_name` varchar(255) NOT NULL,
   `batch_no` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -268,18 +329,18 @@ CREATE TABLE `stock` (
 -- Dumping data for table `stock`
 --
 
-INSERT INTO `stock` (`shop_id`, `product_id`, `batch_no`, `quantity`) VALUES
-('koshal_sk@gmail.com', 1, 1, 898),
-('koshal_sk@gmail.com', 1, 2, 1000),
-('koshal_sk@gmail.com', 3, 1, 1000),
-('koshal_sk@gmail.com', 4, 1, 1000),
-('koshal_sk@gmail.com', 5, 1, 1000),
-('koshal_sk@gmail.com', 6, 1, 992),
-('koshal_sk@gmail.com', 7, 1, 998),
-('koshal_sk@gmail.com', 8, 1, 1000),
-('koshal_sk@gmail.com', 1, 6, 930),
-('koshal_sk@gmail.com', 2, 1, 80),
-('dibya_sk@gmail.com', 7, 1, 1);
+INSERT INTO `stock` (`shop_id`, `product_id`, `medicine_name`, `batch_no`, `quantity`) VALUES
+('koshal_sk@gmail.com', 1, '', 1, 898),
+('koshal_sk@gmail.com', 1, '', 2, 1000),
+('koshal_sk@gmail.com', 3, '', 1, 1000),
+('koshal_sk@gmail.com', 4, '', 1, 1000),
+('koshal_sk@gmail.com', 5, '', 1, 1000),
+('koshal_sk@gmail.com', 6, '', 1, 992),
+('koshal_sk@gmail.com', 7, '', 1, 998),
+('koshal_sk@gmail.com', 8, '', 1, 1000),
+('koshal_sk@gmail.com', 1, '', 6, 930),
+('koshal_sk@gmail.com', 2, '', 1, 80),
+('dibya_sk@gmail.com', 7, '', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -306,7 +367,7 @@ INSERT INTO `users` (`id`, `user_id`, `password`, `name`, `type`, `address`, `co
 (1, 'alex@gmail.com', '123456', 'Alex Alex', 1, 'Rourkela', '8339895678', '2018/04/05'),
 (2, 'zanderzander@gmail.com', '123456', 'zander zander', 1, 'Rourkela', '9777511898', '2018/04/05'),
 (3, 'peter@gmail.com', '123456', 'Peter', 2, 'bbsr', '955612100', '2017-12-22'),
-(4, 'alex@gmail.com', '123456', 'Alex', 2, 'bbsr', '8795612100', '2017-08-16'),
+(4, 'alexalex@gmail.com', '123456', 'Alex', 2, 'bbsr', '8795612100', '2017-08-16'),
 (5, 'doctor1@gmail.com', '123456', 'doctor_one', 3, 'rourkela', '906832521', '2017-10-23'),
 (6, 'doctor2@gmail.com', '123456', 'doctor_two', 3, 'rourkela', '906212521', '2017-12-04'),
 (7, 'test2@test.com', '123456', 'zander zander', 4, 'Rourkela', '8339895678', '2018/04/08'),
@@ -353,10 +414,22 @@ ALTER TABLE `bill_medicine`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `inn`
+--
+ALTER TABLE `inn`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- Indexes for table `medicine`
 --
 ALTER TABLE `medicine`
   ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `NewPres`
+--
+ALTER TABLE `NewPres`
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indexes for table `ordered_medicine`
@@ -405,22 +478,34 @@ ALTER TABLE `bill_medicine`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
+-- AUTO_INCREMENT for table `inn`
+--
+ALTER TABLE `inn`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `medicine`
 --
 ALTER TABLE `medicine`
   MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT for table `NewPres`
+--
+ALTER TABLE `NewPres`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `ordered_medicine`
 --
 ALTER TABLE `ordered_medicine`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `prescriptions`
